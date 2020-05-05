f3a26dc845adb0205718e38953c0385cfc461cbc
Anil-Bhimwal <anil8175789@gmail.com>
2020-05-05 22:42:40 +0530

    week 2 first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React , {Component} from 'react';
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import { DISHES } from './shared/dishes';
-import Menu from './components/MenuComponent';
+import Main from './components/MainComponent';
 
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
+  
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Confusion </NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+        <Main />
       </div>
     );
   }
